3c969a6fb64a4918e20742f17476e146c6f274fa
itsalexi <alexicanamo@gmail.com>
2026-02-09 09:32:34 +0800

    feat: create recipe list view and templates

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import recipe_list
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('recipes/list', recipe_list, name='index'),
 ]
 
 app_name = "ledger"
--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('recipes/', include('ledger.urls', namespace="ledger"))
+    path('', include('ledger.urls', namespace="ledger"))
 ]
