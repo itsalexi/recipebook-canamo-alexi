0697f5a1cd4aab50a9d2de90d1932623ae2d5290
itsalexi <alexicanamo@gmail.com>
2026-02-19 15:09:14 +0800

    feat: create models, function views, modified detail view for recipes

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from datetime import datetime
+from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def get_absolute_url(self):
+        return reverse('ledger:ingredient', args=[str(self.name)])
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def get_absolute_url(self):
+        return reverse('ledger:recipe', args=[str(self.name)])
+
+
+class RecipeIngredient(models.Model):
+
+    quantity = models.CharField(max_length=100)
+    ingredient = models.ForeignKey(
+        Ingredient, on_delete=models.CASCADE, related_name="recipe")
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="ingredients")
+
+    def __str__(self):
+        return f"{self.ingredient} - {self.quantity}"
