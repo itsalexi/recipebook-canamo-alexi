ff4a67eed0062de0c22040a63895820c8a62e9a0
itsalexi <alexicanamo@gmail.com>
2026-03-02 08:42:43 +0800

    feat: add new user model and updated recipe models, fix migrations

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -2,8 +2,15 @@ from django.db import models
 from datetime import datetime
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    short_bio = models.TextField(blank=True)
 
 
 class Ingredient(models.Model):
@@ -18,6 +25,10 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="recipe_author")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name}"
